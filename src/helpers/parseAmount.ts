2bdd8391b9beebe174b7c470b843095ffb6d57b6
Tudor Morar <tudor.morar@multiversx.com>
2025-09-16 12:15:38 +0300

    Refactor parseAmount to mimic sdk-core v.14

--- src/helpers/parseAmount.ts
+++ src/helpers/parseAmount.ts
@@ -1,14 +1,7 @@
 import BigNumber from 'bignumber.js';
 import { DECIMALS } from '../constants';
 
-export function parseAmount(amount: string, numDecimals: number = DECIMALS) {
-  const amountBN = new BigNumber(amount);
-  const multiplier = new BigNumber(10).pow(numDecimals);
-  const result = amountBN.multipliedBy(multiplier);
-
-  if (!result.isInteger()) {
-    throw new Error('Amount has too many decimal places');
-  }
-
+export function parseAmount(amount: string, numDecimals = DECIMALS): string {
+  const result = new BigNumber(amount).shiftedBy(numDecimals).decimalPlaces(0);
   return result.toFixed(0);
 }
